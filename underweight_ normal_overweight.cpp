8df125bdc6dc5a4ef0c5c01761e8fedbf9518644
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2017-11-05 23:18:20 -0800

    Berat dan tinggi badan

--- underweight_ normal_overweight.cpp
+++ underweight_ normal_overweight.cpp
@@ -11,4 +11,13 @@ int main()
     cout << "Silahkan masukkan berat badan(kg) : " ;
     cin >> a;
     
-}
+    if (a<sb/2.5)
+       cout << "  Anda Mengalami : Underweight(kekurangan berat badan)" << endl;
+            else if (sb/2.3 < a)
+                 cout << "  Anda Mengalami : Overweight(kelebihan berat badan)" << endl;
+            else if (sb/2.5 <= a <= sb/2.3)
+           		 cout << "  congratulation!!! : Normal" << endl;
+                 
+                 
+                 return 0;
+    }
