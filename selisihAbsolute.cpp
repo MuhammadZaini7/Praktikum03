5924b5076698224aab7da3ffe5fe1fb7283edb33
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2017-11-05 21:16:24 -0800

    selisih

--- selisihAbsolute.cpp
+++ selisihAbsolute.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main(){
+	int a,b,selisih;
+	cout<<" Bilangan a : "; cin>>a;
+	cout<<" Bilangan b : "; cin>>b;
 
+if (a>b){
+	selisih=a-b;
+	
+	cout<<"Selisih nya"<<selisih;
+}
+
+else if (a>b){
+	selisih=a-b;
+
+	cout<<"Selisih Nilai nya"<<selisih;
+}
+	return 0;
 }
