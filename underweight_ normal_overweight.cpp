7e3c03d3800f12014804f980a4eb83a7880f99b3
MuhammadZaini7 <32381402+MuhammadZaini7@users.noreply.github.com>
2017-11-05 23:17:25 -0800

    Masukkan tinggi dan berat

---
+++ underweight_ normal_overweight.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+   float a;
+    int sb;
+    
+    cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>sb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+    cin >> a;
+    
+}
